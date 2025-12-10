03c22dc6287bb25f442b805be1535a9501048341
ZiloTech <uwamimcdonald@gmail.com>
2025-12-10 23:44:37 +0200

    Update BaseEntity.java

--- src/main/java/com/brelinx/edusuite/model/BaseEntity.java
+++ src/main/java/com/brelinx/edusuite/model/BaseEntity.java
@@ -1,26 +1,23 @@
 package com.brelinx.edusuite.model;
 
-import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
 @Getter
 @Setter
 public abstract class BaseEntity {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id;
     
-    @CreationTimestamp
-    @Column(updatable = false)
+    @CreatedDate
     private LocalDateTime createdAt;
     
-    @UpdateTimestamp
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 }
